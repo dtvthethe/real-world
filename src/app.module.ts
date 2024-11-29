3dd8ab464d5fe5b9710feff1d66ebf6ac016c884
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2024-11-30 03:25:54 +0700

    add-env-file

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,19 +9,28 @@ import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
 import { UserFollow } from './users/user_follow.entity';
 import { ProfileModule } from './profile/profile.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'db',
-      port: 3306,
-      username: 'root',
-      password: 'Aa@123456',
-      database: 'real-world',
-      entities: [Tag, User, Article, Comment, UserFollow],
-      synchronize: true,// disable for production
-      // logging: true
+    ConfigModule.forRoot({// config for env file
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({// config for database
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Tag, User, Article, Comment, UserFollow],
+        synchronize: true,// disable for production
+        // logging: true
+      }),
     }),
     TagsModule,
     ArticlesModule,
